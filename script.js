722720e7279377d0818f110d3f49ab59103bf810
papiquieropizzaya <149841276+papiquieropizzaya@users.noreply.github.com>
2024-08-14 21:16:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 window.addEventListener("load", function() {
-    // Ocultar el loader después de 4 segundos
-    setTimeout(function() {
-        document.getElementById("loader-wrapper").style.display = "none";
-        document.getElementById("content").style.display = "block";
-    }, 4000);
+    const loader = document.getElementById("loader");
+    const wrapper = document.getElementById("loader-wrapper");
+
+    loader.addEventListener("animationiteration", function() {
+        // Una vez que se complete la vuelta actual, realiza la transición hacia arriba
+        wrapper.style.transform = "translateY(-100%)"; // Mueve el fondo hacia arriba
+        document.getElementById("content").style.display = "block"; // Muestra el contenido
+        
+        // Espera 1 segundo para que la transición termine antes de ocultar el wrapper
+        setTimeout(function() {
+            wrapper.style.display = "none";
+        }, 1000);
+    });
+
+    // Desactiva la animación infinita cuando la página está completamente cargada
+    loader.style.animationIterationCount = "1";
 });
