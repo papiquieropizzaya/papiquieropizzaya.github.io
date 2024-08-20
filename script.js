9b484e13112a5b78eaa4ebcb536e23c95145e904
papiquieropizzaya <149841276+papiquieropizzaya@users.noreply.github.com>
2024-08-20 18:01:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,50 @@ window.addEventListener("load", function() {
     // Desactiva la animación infinita cuando la página está completamente cargada
     loader.style.animationIterationCount = "1";
 });
+
+
+
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+
+burger.addEventListener('click', () => {
+    // Toggle Nav
+    nav.classList.toggle('nav-active');
+
+    // Animate Links
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        }
+    });
+
+    // Burger Animation
+    burger.classList.toggle('toggle');
+});
+
+
+
+
+
+
+
+const navbar = document.getElementById('navbar');
+let lastScrollTop = 0;
+
+window.addEventListener('scroll', () => {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        // Scroll hacia abajo, esconder navbar
+        navbar.classList.add('navbar-hidden');
+    } else {
+        // Scroll hacia arriba, mostrar navbar
+        navbar.classList.remove('navbar-hidden');
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Evita que lastScrollTop sea negativo
+});
